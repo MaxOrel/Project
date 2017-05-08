f6d496c3708cda6a21f85b562ba997c2c340f17d
DESKTOP-MPDKHMC\maxim <maxim_91@inbox.ru>
2017-05-08 14:07:10 +0300

    slider added and blog animation / form validation

--- gulp/paths/app.js
+++ gulp/paths/app.js
@@ -8,5 +8,11 @@ module.exports = [
   './source/js/blur.js',
   './source/js/arrow-top.js',
   './source/js/hamburger.js',
-  './source/js/flip.js'
+  './source/js/flip.js',
+  './source/js/preloader.js',
+  './source/js/blog.js',
+  './source/js/textanimation.js',
+  './source/js/slider.js',
+  './source/js/form-valid.js',
+  './source/js/login.js'
 ];
